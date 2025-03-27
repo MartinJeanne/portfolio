fc8b1d5c3f9666fbe7fdb176f548505e49acebf4
MartinJeanne <martinjeanne.dev@gmail.com>
2025-03-27 12:32:01 +0100

    projectCard submenu

--- src/components/cards/ProjectsCard.tsx
+++ src/components/cards/ProjectsCard.tsx
@@ -1,6 +1,9 @@
-import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
+import { CgWebsite } from "react-icons/cg";
 import { BaseCard } from "./BaseCard";
 import styled from '@emotion/styled';
+import type { MenuProps } from 'antd';
+import { Dropdown, Space } from 'antd';
 
 const ProjectTitleLink = styled.a`
     font-size: 1.17em;
@@ -17,25 +20,37 @@ const ProjectContainer = styled.div`
     padding-bottom: 20px;
 `;
 
+interface Project {
+  projectLink: string;
+  codeLink: string;
+  title: string;
+  content: string;
+  techno: string;
+}
+
 export const ProjectCard = () => {
-  const projects = [
+  const projects: Project[] = [
     {
-      src: 'https://github.com/MartinJeanne/linkstart-bot',
+      projectLink: 'https://discord.com/oauth2/authorize?client_id=784536536459771925&permissions=8&scope=bot',
+      codeLink: 'https://github.com/MartinJeanne/linkstart-bot',
       title: 'Bot discord DJ',
       content: 'Un bot discord qui peut de jouer de la musique dans les salons vocaux',
-      techno: 'TypeScript, Java, Spring Boot, Docker'
+      techno: 'TypeScript, Java, Spring Boot, Docker',
     },
     {
-      src: 'https://github.com/MartinJeanne/linkstart-bot',
+      projectLink: 'https://whosbigger.martinjeanne.com/',
+      codeLink: 'https://github.com/MartinJeanne/whosbigger-front',
       title: 'Who\'s bigger',
       content: 'Une application web où le but est trouver la plus grande ville parmi deux villes de Normandie',
-      techno: 'TypeScript, React, Java, Spring Boot, Docker'
+      techno: 'TypeScript, React, Java, Spring Boot, Docker',
+
     },
     {
-      src: 'https://github.com/MartinJeanne/linkstart-bot',
+      projectLink: 'https://l1nkstart.web.app/',
+      codeLink: 'https://github.com/MartinJeanne/live-chat',
       title: 'Live chat',
       content: 'Une application web de messagerie instantanée',
-      techno: 'JavaScript, Vue.js, Firebase, GCP'
+      techno: 'JavaScript, Vue.js, Firebase, GCP',
     },
   ];
 
@@ -46,15 +61,45 @@ export const ProjectCard = () => {
       justifyContent="flex-start"
       hoverContent={
         <>
-          {projects.map((project) => (
-            <ProjectContainer key={project.title}>
-              <ProjectTitleLink href={project.src} target="_blank" rel="noopener noreferrer">
-                {project.title} <FaExternalLinkAlt size={17} />
-              </ProjectTitleLink>
-              <p>{project.content}</p>
-              <p style={{ color: '#ff4757' }}>{project.techno}</p>
-            </ProjectContainer>
-          ))}
+          {projects.map((project) => {
+            // Créer un menu personnalisé pour chaque projet
+            const items: MenuProps['items'] = [
+              {
+                key: '1',
+                label: (
+                  <a target="_blank" rel="noopener noreferrer" href={project.projectLink}>
+                    Go to project
+                  </a>
+                ),
+                icon: <CgWebsite size={24} />,
+                disabled: false,
+              },
+              {
+                key: '2',
+                label: (
+                  <a target="_blank" rel="noopener noreferrer" href={project.codeLink}>
+                    Go to code
+                  </a>
+                ),
+                icon: <FaGithub size={24} />,
+                disabled: false,
+              }
+            ];
+
+            return (
+              <ProjectContainer key={project.content}>
+                <Dropdown menu={{ items }}>
+                  <ProjectTitleLink onClick={(e) => e.preventDefault()}>
+                    <Space>
+                      {project.title} <FaExternalLinkAlt size={17} />
+                    </Space>
+                  </ProjectTitleLink>
+                </Dropdown>
+                <p>{project.content}</p>
+                <p style={{ color: '#ff4757' }}>{project.techno}</p>
+              </ProjectContainer>
+            );
+          })}
         </>
       }
     />
--- todo.md
+++ todo.md
@@ -1,4 +1,7 @@
-put links to projects in projects card
+Link front&back in README.md, on my GitHub
+Improve README.md of this project on the ones I show here
 put icons instead of text for techno used in project card
 Add a card that allow user to send a msg, draw something? See drawer on antd
 skill card need changement
+change website icon
+Make sure Linkstart bot is usable by everyone
