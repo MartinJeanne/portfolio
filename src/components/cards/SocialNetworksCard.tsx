25e54ede9a79a9bda20eac9eda902baad6f53824
MartinJeanne <martinjeanne.dev@gmail.com>
2025-03-31 12:18:03 +0200

    socialNetworksCard translated in english

--- src/components/cards/SocialNetworksCard.tsx
+++ src/components/cards/SocialNetworksCard.tsx
@@ -1,6 +1,7 @@
 import { FaGithub, FaGlobe, FaLinkedin, FaMailBulk } from "react-icons/fa";
 import { BaseCard } from "./BaseCard";
 import styled from '@emotion/styled';
+import { useTranslation } from 'react-i18next';
 
 const SocialLink = styled.a`
   display: flex;
@@ -22,28 +23,31 @@ const SocialLink = styled.a`
 `;
 
 
-export const SocialNetworksCard = () => (
+export const SocialNetworksCard = () => {
+  const { t } = useTranslation();
+
+  return (
   <BaseCard
-    title="Mes réseaux"
+    title=''
     defaultContent={
       <div style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', gap: '15px' }}>
         <SocialLink href="mailto:martinjeanne.dev@gmail.com">
           <FaMailBulk size={24} />
-          <span>Mon email</span>
+          <span>{t('socialNetworksCard.dc.mail')}</span>
         </SocialLink>
         <SocialLink href="https://linkedin.com/in/MartinJeanne" target="_blank" rel="noopener noreferrer">
           <FaLinkedin size={24} />
-          <span>Mon profil Linkedin</span>
+          <span>{t('socialNetworksCard.dc.linkedin')}</span>
         </SocialLink>
         <SocialLink href="https://github.com/MartinJeanne" target="_blank" rel="noopener noreferrer">
           <FaGithub size={24} />
-          <span>Mon profil Github</span>
+          <span>{t('socialNetworksCard.dc.github')}</span>
         </SocialLink>
         <SocialLink href="https://leetcode.com/Martin-jnne" target="_blank" rel="noopener noreferrer">
           <FaGlobe size={24} />
-          <span>Profil LeetCode</span>
+          <span>{t('socialNetworksCard.dc.leetcode')}</span>
         </SocialLink>
       </div>
     }
   />
-); 
+)};
