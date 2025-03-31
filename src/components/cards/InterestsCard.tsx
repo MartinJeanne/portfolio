25cb71eca72f0f52050f8cbc990ba9410acf53e4
MartinJeanne <martinjeanne.dev@gmail.com>
2025-03-31 12:06:54 +0200

    interestCard translated in en

--- src/components/cards/InterestsCard.tsx
+++ src/components/cards/InterestsCard.tsx
@@ -1,26 +1,31 @@
 import { MdInterests } from "react-icons/md";
 import { BaseCard } from "./BaseCard";
+import { useTranslation } from 'react-i18next';
 
-export const InterestsCard = () => (
-  <BaseCard
-    variant="dark"
-    icon={<MdInterests color="white" />}
-    title="Intéressé par :"
-    subtitle="React, l'IA, la blochaine et les chiots."
-    hoverContent={
-      <>
-        <h2 style={{ color: 'white' }}>Centres d'intérêt</h2>
-        <div style={{ marginTop: '15px', color: 'white' }}>
-          <h3>React</h3>
-          <p>Développement d'applications modernes</p>
-          <h3 style={{ marginTop: '10px' }}>Intelligence Artificielle</h3>
-          <p>Machine Learning et Deep Learning</p>
-          <h3 style={{ marginTop: '10px' }}>Blockchain</h3>
-          <p>Smart contracts et DApps</p>
-          <h3 style={{ marginTop: '10px' }}>Chiots</h3>
-          <p>Parce que qui n'aime pas les chiots ?</p>
-        </div>
-      </>
-    }
-  />
-);
+export const InterestsCard = () => {
+  const { t } = useTranslation();
+
+  return (
+    <BaseCard
+      variant="dark"
+      icon={<MdInterests color="white" />}
+      title={t('interestsCard.title')}
+      subtitle={t('interestsCard.subtitle')}
+      hoverContent={
+        <>
+          <h2 style={{ color: 'white' }}>{t('interestsCard.h.title')}</h2>
+          <div style={{ color: 'white' }}>
+            <h3>{t('interestsCard.h.subtitle1')}</h3>
+            <p>{t('interestsCard.h.content1')}</p>
+            <h3 style={{ marginTop: '10px' }}>{t('interestsCard.h.subtitle2')}</h3>
+            <p>{t('interestsCard.h.content2')}</p>
+            <h3 style={{ marginTop: '10px' }}>{t('interestsCard.h.subtitle3')}</h3>
+            <p>{t('interestsCard.h.content3')}</p>
+            <h3 style={{ marginTop: '10px' }}>{t('interestsCard.h.subtitle4')}</h3>
+            <p>{t('interestsCard.h.content4')}</p>
+          </div>
+        </>
+      }
+    />
+  )
+};
